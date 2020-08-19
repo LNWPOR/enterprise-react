b079534d98c111c1cf46c8bbfb29bdd701b3e217
Nuttavut Thongjor <n.thongjor@gmail.com>
2020-08-19 22:18:24 +0700

    Fix PostItem test

--- src/posts/PostItem.spec.tsx
+++ src/posts/PostItem.spec.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { MemoryRouter } from 'react-router-dom';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 
 import PostItem from './PostItem';
 
@@ -9,21 +10,26 @@ jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('PostItem', () => {
-  it('renders PostItem correctly', () => {
+  it('renders PostItem correctly', async () => {
     const post = { id: 1, title: 'Title', body: 'Body' };
 
     mockedAxios.get.mockImplementationOnce(() =>
       Promise.resolve({ data: post })
     );
 
-    const wrapper = shallow(
+    const wrapper = mount(
       <MemoryRouter initialEntries={['/posts', `${post.id}`]}>
         <PostItem />
       </MemoryRouter>
     );
 
+    await act(async () => {
+      await new Promise((resolve) => setImmediate(resolve));
+      wrapper.update();
+    });
+
     for (const value of Object.values(post)) {
-      expect(wrapper.exists(`dd[children='${value}']`));
+      expect(wrapper.text().includes(`${value}`)).toBe(true);
     }
   });
 });
